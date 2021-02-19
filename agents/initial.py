4bc42bca14bc7719302c7c9fc69b8681a261a86a
lolotrgeek <lolotrgeek@gmail.com>
2021-02-19 09:36:48 -0600

    remove signal handler, add publish

--- agents/initial.py
+++ agents/initial.py
@@ -1,18 +1,27 @@
 
+import sys
+import signal
 import time
+
 from modules.publish import Publish
 from modules.reply import Reply
 
 ENERGY = 1000
 NAME = "AGENT0"
 
+
+def signal_handler(signal, frame):
+    print("\nprogram exiting gracefully")
+    sys.exit(0)
+
+
 def Start():
     print('Listening...')
     while True:
-        # Publish(NAME, {"energy": ENERGY})
         Reply(Response)
+        Publish(NAME, {"energy": ENERGY})
 
-        
+
 def Response(request):
     if isinstance(request, object) & isinstance(request.get("energy"), int):
         print(request.get("energy"))
