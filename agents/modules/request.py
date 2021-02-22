64b8457253409e585bc04740fee9a9b32a94cf87
lolotrgeek <lolotrgeek@gmail.com>
2021-02-22 07:52:45 -0600

    add callback

--- agents/modules/request.py
+++ agents/modules/request.py
@@ -10,7 +10,7 @@ socket.setsockopt(zmq.LINGER, 0)
 # https://stackoverflow.com/questions/7538988/zeromq-how-to-prevent-infinite-wait
 
 
-def Request(message, wait=5000):
+def Request(message, callback, wait=5000):
     socket.setsockopt(zmq.RCVTIMEO, wait)
     print("Sending request", message)
     socket.send_json(message)
