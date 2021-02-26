6e9ea581012dd576089658ddcc52684c24e5bdf7
lolotrgeek <lolotrgeek@gmail.com>
2021-02-26 09:26:16 -0600

    for reference, remove in favor of environment.py

--- agents/run.py
+++ agents/run.py
@@ -7,3 +7,11 @@ actions = ["action!"]
 
 cmd = f'python agents/agent.py --life {life} --address {address} --survival {survival} --actions {actions}'
 subprocess.run(cmd)
+
+AGENTS = []
+
+subprocess.run('python Agents/initial.py')
+# load each Agent into a sub process
+for Agent in AGENTS:
+    cmd = f'python Agents/Agent.py --life {Agent.life} --address {Agent.address} --survival {Agent.survival} --actions {Agent.actions}'
+    subprocess.run(cmd)
